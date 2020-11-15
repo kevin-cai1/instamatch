d949cec505813b58bf0c5e437382fca75c540780
Charmaine Leung <charmaineleung8@gmail.com>
2020-11-16 00:25:51 +1100

    created title bar

--- instamatch/Friends.js
+++ instamatch/Friends.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import { Button, WhiteSpace, WingBlank } from '@ant-design/react-native';
 
 const Friends = () => {
- return <Text>This is friends home</Text>;
+ return (
+   <View>
+    <View style={friends.header}>
+     <Text style={friends.title}>Friends</Text>
+    </View>
+    <Text>You have:</Text>
+    <Button type="primary">primary</Button>
+    <WhiteSpace />
+   </View>
+ );
 };
+
+const friends = StyleSheet.create({
+ header: {
+  borderBottomColor: 'black',
+  borderBottomWidth: 1,
+  height: 50,
+ },
+ title: {
+  textAlign: 'center',
+  fontSize: 30,
+  paddingTop: 10,
+ }
+});
 
 export default Friends;
