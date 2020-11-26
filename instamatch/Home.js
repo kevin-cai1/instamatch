c713572b2bbd955ddbdb28a893228ee605919b1a
Miran Nakamura <mnakamura1998@gmail.com>
2020-11-26 22:02:39 +1100

    home updates

--- instamatch/Home.js
+++ instamatch/Home.js
@@ -1,8 +1,52 @@
 import React from 'react';
-import {Text} from 'react-native';
+import * as Font from 'expo-font';
+import { Image, Text, View } from 'react-native';
+import { List, Button, WhiteSpace } from '@ant-design/react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+const Item = List.Item;
+const Activities = [
+  [
+    {
+      label: 'Gym',
+      value: 'gym',
+    },
+    {
+      label: 'Brunch',
+      value: 'brunch',
+    },
+  ],
+  [
+    {
+      label: 'Video Call',
+      value: 'video call',
+    },
+    {
+      label: 'Dinner',
+      value: 'dinner',
+    },
+  ],
+];
 
 const Home = () => {
- return <Text>This is the home screen</Text>;
+  return (
+    <View>
+      <View>
+        <Text>Home</Text>
+      </View>
+      <List style={{ backgroundColor: 'white' }} className="picker-list" renderHeader={'Session Details'}>
+        <Item arrow="empty" onPress={() => {}}>
+          I'm free for...
+        </Item>
+        <Item extra="(Default) Any Activity" arrow="empty" onPress={() => {}}>
+          To do...
+        </Item>
+        <Item extra="(Default) All Friends" arrow="empty" onPress={() => {}}>
+          With...
+        </Item>
+      </List>
+      <Button type="primary">Start Now!</Button>
+    </View>);
 };
 
 export default Home;
