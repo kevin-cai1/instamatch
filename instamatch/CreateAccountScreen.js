2349e786607d7e8cd04fda81a3dc2cd08411fcea
johnlikescode <johnisakim@gmail.com>
2020-12-02 16:17:58 +1100

    Styling ALMOST completed - image messed up

--- instamatch/CreateAccountScreen.js
+++ instamatch/CreateAccountScreen.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Image, View, TouchableWithoutFeedback, Keyboard, Text, TextInput } from 'react-native';
-import { InputItem, Button } from '@ant-design/react-native';
+import { StyleSheet, Image, View, TouchableWithoutFeedback, Keyboard, Text, TextInput, TouchableOpacity } from 'react-native';
 import InstaMatchLogo from './assets/InstaMatchLogo.png';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Formik } from 'formik';
@@ -75,7 +74,7 @@ const CreateAccountScreen = ({navigation}) => {
  return (
    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-      <Image source={InstaMatchLogo} style={styles.logo}/>
+        <Image source={InstaMatchLogo} style={styles.logo}/>
         <Formik
           initialValues={{ username: '', email: '', password: '', confirmPassword: ''}}
           validationSchema={AccountDetailValidation}
@@ -89,44 +88,52 @@ const CreateAccountScreen = ({navigation}) => {
         >
           {(props) => (
             <View style={styles.form}>
-              <InputItem 
+              <TextInput 
                 placeholder="Username" 
-                styles={styles.testBox}
+                style={styles.inputBox}
                 value={props.values.username}
                 onChangeText={props.handleChange('username')}
+                placeholderTextColor='#647C90'
               />
               <Text style={styles.errorText}>{props.touched.username && props.errors.username}</Text>
-             <InputItem 
+             <TextInput 
                 placeholder="Email" 
-                styles={styles.inputBox}
+                style={styles.inputBox}
                 type='email'
                 value={props.values.email}
                 onChangeText={props.handleChange('email')}
+                placeholderTextColor='#647C90'
               />
               <Text style={styles.errorText}>{props.touched.email && props.errors.email}</Text>
-              <InputItem 
+              <TextInput 
                 placeholder="Password" 
-                styles={styles.inputBox}
+                style={styles.inputBox}
                 type='password'
                 value={props.values.password}
                 onChangeText={props.handleChange('password')}
+                placeholderTextColor='#647C90'
               />
               <Text style={styles.errorText}>{props.touched.password && props.errors.password}</Text>
-              <InputItem 
+              <TextInput 
                 placeholder="Confirm Password" 
-                styles={styles.inputBox}
+                style={styles.inputBox}
                 type='password'
                 value={props.values.confirmPassword}
                 onChangeText={props.handleChange('confirmPassword')}
+                placeholderTextColor='#647C90'
               />
               <Text style={styles.errorText}>{props.touched.confirmPassword && props.errors.confirmPassword}</Text>
-              <Button onPress={props.handleSubmit} type="primary" style={styles.button}>Create Account</Button>
+              <TouchableOpacity onPress={props.handleSubmit} style={styles.createAccountButton}>
+                <Text style={styles.buttonText}>Create Account</Text>
+              </TouchableOpacity>
             </View>
           )}
         </Formik>
         <View>
-          <Text>Already have an account?</Text>
-          <Button onPress={() => navigation.navigate('LoginScreen')}>Sign In</Button>
+          <Text style={styles.font}>Already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')} style={styles.signInButton}>
+          <Text style={styles.signInText}>Sign In</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </TouchableWithoutFeedback>
@@ -139,43 +146,71 @@ const styles = StyleSheet.create({
     backgroundColor: '#F2F2F2',
     margin: 20,
     marginTop: 30,
+    alignItems: 'center'
   },
   form: {
     width: 300,
     flex: 1,
+    justifyContent: 'center',
     alignSelf: 'center'
   },
   inputBox: {
     height: 40,
-    borderColor: '#cccccc', 
+    borderColor: '#647C90', 
     borderWidth: 1, 
     borderRadius: 13, 
     paddingHorizontal: 10, 
-    marginTop: 10
+    marginTop: 10,
+    fontSize: 18,
   },
   logo: {
-    alignSelf: "center"
+    marginTop: 50,
+    alignContent: 'center'
   },
-  button: {
-    backgroundColor: "#1C3AA1",
-    alignSelf: "flex-end"    
+  createAccountButton: {
+    borderColor:'#1C3AA1',
+    alignItems:'center',
+    justifyContent:'center',
+    width:200,
+    height:44,
+    backgroundColor:'#1C3AA1',
+    borderRadius:24,
+    alignSelf: 'flex-end',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowColor: '#474e59',
+    shadowOffset: { height: 3, width: 0 }   
+  },
+  signInButton: {
+    alignItems:'center',
+    alignSelf:'center',
+    justifyContent:'center',
+    width:132,
+    height:44,
+    marginTop: 18,
+    marginBottom: 30,
   },
   errorText: {
     color: 'crimson',
-    fontWeight: 'bold',
     marginBottom: 10,
     marginTop: 6,
     fontSize: 16,
     textAlign: 'center'
   },
-  testBox: {
-    borderWidth: 1,
-    borderColor: 'crimson',
-    padding: 10,
+  buttonText: {
+    fontWeight: 'bold',
     fontSize: 18,
-    borderRadius: 10,
-    height: 40,
-    marginBottom: 40,
+    color: '#FFFFFF'
   },
+  signInText: {
+    color: '#1C3AA1',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
+  },
+  font: {
+    fontSize: 18,
+    alignSelf: 'center',
+  }
 })
 export default CreateAccountScreen;
